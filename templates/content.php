bcd18387b8905660281607a4a1056ce1d35e4084
Miquel Adell Borràs <miquel.adell@gmail.com>
2016-03-03 13:11:59 +0100

    sasssify

--- templates/content.php
+++ templates/content.php
@@ -1,10 +1,38 @@
 <article <?php post_class(); ?>>
 
-    <header>
-      <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-summary">
+  <header>
+    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <?php get_template_part('templates/entry-meta'); ?>
+  </header>
+
+  <div class="row">
+
+      <?php  $args = array(
+                  'post_parent'    => $post->ID,
+                  'post_type'      => 'attachment',
+                  'numberposts'    => -1, // show all
+                  'post_status'    => 'any',
+                  'post_mime_type' => 'image',
+                  'orderby'        => 'menu_order',
+                  'order'           => 'ASC'
+             );
+
+      $images = get_posts($args);
+      $textCss = "col-sm-8 col-sm-offset-2";
+      if($images) {
+          $textCss = "has-picture col-sm-8";
+        ?>
+        <div class="images col-sm-4 images-column">
+          <?php foreach($images as $image) { ?>
+            <img src="<?php echo wp_get_attachment_url($image->ID); ?>" alt="<?php echo $image->post_excerpt; ?>"></img>
+          <?php } ?>
+        </div>
+      <?php } ?>
+
+
+
+    <div class="entry-summary text-center <?=$textCss?> ">
       <?php the_excerpt(); ?>
+    </div>
   </div>
 </article>
